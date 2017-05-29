7af6c984373cebfbb1df9ed565382730b3f5ad84
rvfeed <akhil406@gmail.com>
2017-05-29 11:35:27 +0530

    improved indexedb code

--- budget.js
+++ budget.js
@@ -4,81 +4,76 @@
       //  console.log(new Object("hi").toString())        
         function DataStorage(){
             this.data = [];            
-        }         var store = new DataStorage();
+        }      
+           var store = new DataStorage();
            
         DataStorage.prototype.searchData = function(input){                
                 return this.data[input.toString()];
             }
         function Validation(){         
-            var form = [];                        
-                      
+            var form = [];                      
         }
-          var valid = new Validation();
-        
+
+        var valid = new Validation();        
         Validation.prototype.formValidation = function(d){
-                var formDup = {};
+                var formDup = {};               
                 for( var x = 0; x < d.length-1; x++){
                     if(d[x].value == ""){
                         alert("Please enter "+d[x].name);
                         return false;
                         break;
-                    }                    
-                    
+                    }                   
                     if(d[x].name == "itemDate"){
                         var date = d[x].value;                         
                     }
-
                     formDup[d[x].name] = d[x].value
                 }                
-                // if(!store.data[date.toString()]) store.data[date.toString()] =  [];                
-                //    store.data[date.toString()].push(formDup);;
-                formDup.id = date;
-                   add(formDup);
+                formDup.id = date;                
+                read(date, function(result){   
+                    console.log(result);  
+                    result.push(formDup)               
+                    result = {"id":date, "data": result};                    
+                    add(result);
+                });                   
                 return true;
             }  
         function Display(){
-           var HtmlOutput = function(data){               
+           var HtmlOutput = function(data){   
+               console.log(data)            
                 return data.itemName + "   "+ data.itemPrice+"   "+ data.itemDate+"\n";     
             }
              this.tempFunc = function(data){
-               // for(var x in data){
-                    //console.log(data[i][x]);
-                    console.log(data)
                     var node = document.createElement("div");
                     var tnode = document.createTextNode(HtmlOutput(data));
                     node.appendChild(tnode);
                     document.getElementById("result").appendChild(node);
-               // }
             }
-            this.showData = function(data){                
+            this.showData = function(data){   
+                console.log(data)             
                 document.getElementById("result").innerHTML = "";
                 for(var i in data){           
                    this.tempFunc(data[i]);             
                 }                
             }
             
-        }
-        
+        }        
                  
         var disp = new Display();
-console.dir(disp)
           
         function save(){
           //  add(); return;
             var formSize = document.budget.length-1;                        
            if(!valid.formValidation(document.budget)) return false;
-          readAll();
+          
           // disp.showData(store.data);           
         }
         function search(){
             document.getElementById("result").innerHTML = "";
-            disp.tempFunc(store.searchData(document.getElementById("search").value));
+           // alert(document.getElementById("search").value)
+           console.log(store.data);
+            read(document.getElementById("search").value);
         }
 
-        function xyz(a, b, c, d){
-            console.dir(Array.prototype.push.call(arguments, 7))
-            console.log(arguments)
-        }
 var db;        
 function IntiateDb(){
             //prefixes of implementation that we want to test
@@ -103,11 +98,14 @@ request.onerror = function(event) {
 request.onsuccess = function(event) {
   db = request.result;
   console.log("success: "+ db);
+  readAll();
 };
  
 request.onupgradeneeded = function(event) {
         var db = event.target.result;
-        var objectStore = db.createObjectStore("budget", {keyPath: "id"});        
+        var objectStore = db.createObjectStore("budget", {keyPath: "id"});     
+        console.log("okkk")
+    
     }
 //read();
 }
@@ -118,29 +116,33 @@ function add(data) {
                 .add(data);
                                  
         request.onsuccess = function(event) {
-              //  alert("Kenny has been added to your database.");
+                console.log("Kenny has been added to your database.");
         };
          
         request.onerror = function(event) {
             console.log(event)
-                alert("Unable to add data\r\nKenny is aready exist in your database! ");       
+                console.log("Unable to add data\r\nKenny is aready exist in your database! ");       
         }
          
 }
 
-function read() {
+function read(dt, callback) {
         var transaction = db.transaction(["budget"]);
         var objectStore = transaction.objectStore("budget");
-        var request = objectStore.get("00-03");
+        var request = objectStore.get(dt);        
         request.onerror = function(event) {
-          alert("Unable to retrieve daa from database!");
+            console.log("okkErr")
+            if(callback) callback([]);
         };
         request.onsuccess = function(event) {
+            console.log("okkSucc")
+          if(request.result)
+          if(callback) callback((request.result)?request.result.data|| []:[]);
           // Do something with the request.result!
           if(request.result) {
-                alert("Name: " + request.result.name + ", Age: " + request.result.age + ", Email: " + request.result.email);
+                disp.showData(request.result.data || []);
           } else {
-              //  alert("Kenny couldn't be found in your database!"); 
+          if(callback) callback([]);
           }
         };
 }
@@ -152,8 +154,9 @@ function readAll() {
           var cursor = event.target.result;
          
           if (cursor) {
-                console.log(cursor.value);
-                temp.push(cursor.value);
+                console.log(cursor.value.data);
+                for(var x in cursor.value.data)
+                    temp.push(cursor.value.data[x]);
                 //alert("Name for id " + cursor.key + " is " + cursor.value.name + ", Age: " + cursor.value.age + ", Email: " + cursor.value.email);
                 cursor.continue();
           }
@@ -166,4 +169,3 @@ function readAll() {
         };     
 }
 
-xyz(1, 2, 3, 4);
