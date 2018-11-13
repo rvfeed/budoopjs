ca933536e93a4b85c71bdf6ac485e1e1bb1fd070
Boddu, Raja (rb651w) <rb651w@att.com>
2018-11-13 16:13:47 +0530

    added insta download

--- old/js/dataStorage.js
+++
@@ -1,6 +0,0 @@
-function DataStorage(){
-    this.data = [];            
-}           
-DataStorage.prototype.searchData = function(input){                
-    return this.data[input.toString()];
-}
